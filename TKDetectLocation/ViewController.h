ce1a6103568a9eb7836038e343ab3226c07bd76f
“Tushar <tushar.kolhe01@gmail.com>
2016-10-23 19:09:42 +0530

    Initial UI created

---
+++ TKDetectLocation/ViewController.h
@@ -0,0 +1,27 @@
+//
+//  ViewController.h
+//  TKDetectLocation
+//
+//  Created by Felix ITs 04 on 23/10/16.
+//  Copyright © 2016 Tushar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+
+@interface ViewController : UIViewController<MKMapViewDelegate,CLLocationManagerDelegate>
+{
+    CLLocationManager *locationManager;
+}
+
+@property (weak, nonatomic) IBOutlet MKMapView *myMapView;
+@property (weak, nonatomic) IBOutlet UILabel *labelLatitude;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *labelLongitude;
+@property (weak, nonatomic) IBOutlet UIButton *getLocationButton;
+
+@end
+
